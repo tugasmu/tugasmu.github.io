550fa87c52557f8822b64c529007808ae994c001
Muhammad Afrizal <81007150+itzmerzl@users.noreply.github.com>
2026-02-05 13:04:35 +0700

    Update Supabase configuration in env.js

--- env.js
+++ env.js
@@ -7,7 +7,7 @@ window.__ENV__ = {
   APP_NAME: "Tugasmu",
   // Optional: kalau kamu deploy di subfolder (project pages), isi BASE_PATH, contoh: "/tugasmu"
   BASE_PATH: "",
-  SUPABASE_URL: "YOUR_SUPABASE_URL",
-  SUPABASE_ANON_KEY: "YOUR_SUPABASE_ANON_KEY",
+  SUPABASE_URL: "https://stvasgalfivxlwullqgs.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN0dmFzZ2FsZml2eGx3dWxscWdzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAyMzg3NDMsImV4cCI6MjA4NTgxNDc0M30.B13VAQ50PEUAHmPzHavMZr5nOhosI6CpLsr_4nrnBDw",
   STORAGE_BUCKET: "submissions",
 };

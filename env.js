4f3649724a11780eaa6ad45df5810464896656eb
Muhammad Afrizal <81007150+itzmerzl@users.noreply.github.com>
2026-02-05 13:52:00 +0700

    Update Supabase URL and anon key in env.js

--- env.js
+++ env.js
@@ -7,7 +7,7 @@ window.__ENV__ = {
   APP_NAME: "Tugasmu",
   // Optional: kalau kamu deploy di subfolder (project pages), isi BASE_PATH, contoh: "/tugasmu"
   BASE_PATH: "",
-  SUPABASE_URL: "https://stvasgalfivxlwullqgs.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN0dmFzZ2FsZml2eGx3dWxscWdzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAyMzg3NDMsImV4cCI6MjA4NTgxNDc0M30.B13VAQ50PEUAHmPzHavMZr5nOhosI6CpLsr_4nrnBDw",
+  SUPABASE_URL: "https://ojgomkmnqnbyqjyniofi.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9qZ29ta21ucW5ieXFqeW5pb2ZpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAyNjUwNDUsImV4cCI6MjA4NTg0MTA0NX0.NBS9bxAcO8Ys1TzADoamo0OiHayRJ1RFZJrZg8DPm7M",
   STORAGE_BUCKET: "submissions",
 };
